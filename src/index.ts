c382f853d02b080d151ab997e185580883701f4c
Nocky <33870333+cocytus1223@users.noreply.github.com>
2019-10-10 21:16:45 +0800

    泛型

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // import './datetype'
 // import './enum'
 // import './interface'
-import './class'
+// import './class'
+import './generics'
 
 let hello : string = 'Hello TypeScript'
 document.querySelectorAll('.app')[0].innerHTML = hello;
